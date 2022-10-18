f76f586a3c2126b21d73d3592c167fca092a7a6a
Marco Couzin <mcouzin@gmail.com>
2022-10-17 23:50:37 -0300

    Teste commit

--- src/br/com/eleicoes/controller/ControllerCandidatos.java
+++ src/br/com/eleicoes/controller/ControllerCandidatos.java
@@ -3,6 +3,7 @@ package br.com.eleicoes.controller;
 public class ControllerCandidatos {
 	
 	public static void controlStep(int step, String respostaUsuario) {
+
 		switch (step) {
 		case 1:
 			StepUm.executar(respostaUsuario);
